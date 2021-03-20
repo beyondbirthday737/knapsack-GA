a57e29b3bb71da03948d31dc6bd3bd19d07941e8
Gustavo <49646779+beyondbirthday737@users.noreply.github.com>
2021-03-20 14:26:40 -0300

    Add files via upload

---
+++ extract_objects.py
@@ -0,0 +1,13 @@
+class Objects:
+    def __init__(self):
+        self.objects_list = []
+
+        with open("objetos.txt") as f:
+            self.objects_list = f.read().splitlines()
+
+    def get_objects_list(self):
+        return self.objects_list
+
+    def print_objects_list(self):
+        for item in self.objects_list:
+            print(item)
